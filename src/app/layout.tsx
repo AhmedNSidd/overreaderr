8625319140b9b7ea82a68642726ca55f4d15b7ef
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2024-10-12 17:04:51 -0700

    fix: rm user scale disable meta, unused metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,21 +4,13 @@ import AppShell from '@/components/AppShell';
 import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 import { theme } from '../theme';
 
-export const metadata = {
-  title: 'Mantine Next.js template',
-  description: 'I am using Mantine with Next.js!',
-};
-
 export default function RootLayout({ children }: { children: any }) {
   return (
     <html lang="en">
       <head>
         <ColorSchemeScript />
         <link rel="shortcut icon" href="/logo.svg" />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-        />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </head>
       <body>
         <MantineProvider theme={theme}>
